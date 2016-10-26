a1a4cf1b711df9459d6aa5fd0ed60a9c84839b11
xendke@FoxDT <xendke@gmail.com>
2016-10-26 18:32:49 +0000

    improving start.py

--- start.py
+++ start.py
@@ -1,18 +1,17 @@
 import solver
 
-width = eval(input("Width of the matrix (including right hand side): "))
-height = eval(input("Height of the matrix: "))
+width = int(input("Width of the matrix (including right hand side): "))
+height = int(input("Height of the matrix: "))
 matrix = []
 
 for i in range(0, height):
     matrix.append([])
     for j in range(0, width):
         if(j+1 == width):
-            print("Right Hand Side:")
-        t = eval(input("entry at ["+ str(i)+", "+ str(j)+ "]: "))
+            print("-->Right Hand Side:")
+        t = eval(input("->entry at ["+ str(i)+", "+ str(j)+ "]: "))
         matrix[i].append(t)
-    print("Next Row: ")
+    print("<-Next Row: ")
 
 solver.print_matrix(matrix)
-
 solver.solve(matrix)
